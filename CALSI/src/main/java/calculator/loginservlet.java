d45e8c3a67fd953ffef89ed2b51a510ae7284d6f
Codes <rneel936@gmail.com>
2024-09-12 14:30:12 +0530

    this is my first servlet code(calculator)

---
+++ CALSI/src/main/java/calculator/loginservlet.java
@@ -0,0 +1,40 @@
+package calculator;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Loginservlet
+ */
+@WebServlet("/Loginservlet")
+public class loginservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		
+		String uid=req.getParameter("uid");
+		String pwd=req.getParameter("pwd");
+				
+		 out.println("Hello"+uid+ "your password is "+pwd);
+	}
+
+}
